ea6110b0cee0c15de7a30ea30b12c0dd7ad7ab7d
fullstack001 <mr.dimas332@gmail.com>
2025-10-01 21:12:01 -0700

    loader update

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -283,9 +283,9 @@ export default function MembershipApproval(): JSX.Element {
         </div>
 
         {/* Approval Message */}
-        {/* <div className="text-[#452005] text-[18px] font-serif my-2 sm:my-2 md:my-6 font-normal px-4 w-[70vw] max-w-[320px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[400px]">
+        <div className="text-[#452005] text-[18px] font-serif my-2 sm:my-2 md:my-6 font-normal px-4 w-[70vw] max-w-[320px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[400px]">
           Your membership has been approved!
-        </div> */}
+        </div>
 
         {/* Membership Details */}
         <div className="text-[#452005] text-[18px] font-serif my-2 sm:my-2 md:my-6 leading-relaxed w-[70vw] max-w-[320px] sm:max-w-[400px] md:max-w-[400px] lg:max-w-[400px] px-4">
